d1d81c413e0bcf02dae1af93837931a514bd288c
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-09-17 13:28:01 -0400

    update task 3 git

--- setup.R
+++ setup.R
@@ -3,4 +3,9 @@ pres=list.files("pres",full=T,pattern="Rmd")
 lapply(pres,function(x) rmarkdown::render(x))
 
 
+if(F){
+pres_html=list.files("pres",full=T,pattern="html")
+system("cp -R pres/ docs/pres/")
+}
+
 
