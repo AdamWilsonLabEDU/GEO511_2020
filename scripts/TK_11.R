0c9f52175a6b4001181cd0ed90618f4ca2551abd
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-11-05 14:15:06 -0500

    update project tasks

--- scripts/TK_11.R
+++ scripts/TK_11.R
@@ -22,7 +22,7 @@ md_bullet(rmarkdown::metadata$reading)
 #' 
 #' # Tasks
 #' 
-## ----reading,results='asis',echo=F--------------------------------------------
+## ----tasks,results='asis',echo=F----------------------------------------------
 md_bullet(rmarkdown::metadata$tasks)
 
 #' 
@@ -33,7 +33,7 @@ md_bullet(rmarkdown::metadata$tasks)
 #' * Expose you to the work of your peers so you know what others are working on
 #' * Provide an opportunity to share your knowledge to improve their project
 #' 
-#' You should use the project website template (or similar) to generate a html version of your project report. If your project requires any data not available in public repositories, you should put it in a folder called `/data` in your project's home directory and then import it into R with `read.csv('data/filname.csv')` or similar so that anyone with a copy of the repository can re-create the HTML output.
+#' You should use the project website template (or similar) to generate a html version of your project report. If your project requires any data not available in public repositories, you should put it in a folder called `/data` in your project's home directory and then import it into R with `read.csv('data/filename.csv')` or similar so that anyone with a copy of the repository can re-create the HTML output.
 #' 
 #' ## Required components of first draft
 #' 
